b2b956dbe7f8121e6e73cd42a977f24de82e24e9
Van Phan <vanthuyphan@gmail.com>
2018-04-23 12:33:21 -0500

    event sql

--- src/sql/createTables.sql
+++ src/sql/createTables.sql
@@ -10,3 +10,23 @@ CREATE TABLE IF NOT EXISTS `User` (
 ) ENGINE MyISAM DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 insert into User(`email`, `user_name`, `first_name`, `last_name`, `password`) values ('vanthuyphan@gmail.com', 'vanthuyphan', 'van', 'phan', '123456');
 insert into User(`email`, `user_name`, `first_name`, `last_name`, `password`) values ('bati@gmail.com', 'bati', 'bati', 'bati', '123456');
+
+
+CREATE TABLE IF NOT EXISTS `Event` (
+	`id` BIGINT NOT NULL AUTO_INCREMENT,
+	`title` VARCHAR(100),
+	`start_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	`end_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	`begin_location` VARCHAR(50),
+	`end_location` VARCHAR(50),
+	`distance` FLOAT,
+	`comment` VARCHAR(100),
+	`status` VARCHAR(100),
+	`accident_location` VARCHAR(100),
+	`accident_description` VARCHAR(100),
+	`hasAccident` SMALLINT,
+	PRIMARY KEY (`id`)
+) ENGINE MyISAM DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+insert into Event(`title`, `start_date`, `end_date`, `begin_location`, `end_location`, `distance`, `comment`, `status`, `accident_location`, `accident_description`, `hasAccident`)
+values ('Test Event', '2018-04-25', '2018-04-27', 'London', 'Paris', 120, 'Windy Road', 'On Going', '', '', 0);
